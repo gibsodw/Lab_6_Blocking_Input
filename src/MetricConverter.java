1fe73fb415d9e8b6feb3697788c3d7dea56c3ad4
dongi <dongibson633@gmail.com>
2024-10-06 23:13:35 -0400

    New Commit 3

--- src/MetricConverter.java
+++ src/MetricConverter.java
@@ -1,23 +1,22 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class MetricConverter {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        double meters = 0;
-        double miles =  0;
-        double feet = 0;
-        double inches = 0;
-
+        Random rand = new Random();
+        int playerNumber = 0;
+        int computerNumber = rand.nextInt(11);
 
         String trash = ""; // use for bad input which will read as a String
         boolean done = false;
         do {
-            System.out.println("Enter your length in meters: ");
+            System.out.println("Enter your number guess [1 2 3 4 5 6 7 8 9 10] ");
 
             if (in.hasNextDouble()) {
 
                 // OK safe to read in a double
-                meters = in.nextDouble();
+                playerNumber = in.nextInt();
                 in.nextLine(); // clears the newline from the buffer
                 done = true;  // because we got a valid number we can end the loop
             } else {
@@ -29,13 +28,26 @@ public class MetricConverter {
 
         } while (!done);  // initially done is false so !done is true when done is true !done is false
 
-        miles = meters / 1609;
-        feet = meters * 3.281;
-        inches = meters * 39.37;
+        if(playerNumber == computerNumber) {
 
-        System.out.println("Your distance in miles is " + miles);
-        System.out.println("Your distance in feet is " + feet);
-        System.out.println("Your distance in inches is " + inches);
+            System.out.println("The computer number was " + computerNumber);
+            System.out.println("\nYou win" );
+
+        }
+
+        if(playerNumber > computerNumber) {
+
+            System.out.println("The computer number was " + computerNumber);
+            System.out.println("\nToo high " );
+
+        }
+
+        if(playerNumber < computerNumber) {
+
+            System.out.println("The computer number was " + computerNumber);
+            System.out.println("\nToo low " );
+
+        }
 
     }
 }
